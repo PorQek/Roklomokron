702130afc41c12b603236f5a1b06b091247502c5
Por_Que <damianklabuhn@gmail.com>
2020-05-05 19:48:59 +0200

    death screen progress

---
+++ Assets/DeathMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathMenu : MonoBehaviour
+{
+    public static bool PlayerisDead = false;
+
+    public GameObject deathMenuUI;
+
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+       if (PlayerisDead == true)
+        {
+            DeathScreen();
+        }
+       
+    }
+
+    public void Replay()
+    {
+        deathMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        PlayerisDead = false;
+    }
+
+    void DeathScreen()
+    {
+        deathMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        PlayerisDead = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu...");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+    }
+}
