73dd01d2168a043efb4b23cb7b80d0df9fe55d11
Por_Que <damianklabuhn@gmail.com>
2020-05-07 17:29:36 +0200

    Fever implemented to new visuals

--- Assets/World_Movement.cs
+++ Assets/World_Movement.cs
@@ -11,11 +11,19 @@ public class World_Movement : MonoBehaviour
     private bool isAnimating = false;
     private bool isLeft = true;
 
+    public GameObject BackGround;
+    public GameObject FeverBackGround;
+
+    
+    public GameObject FeverWalls;
+
 
 
     void Update()
     {
         WorldMovement();
+        BackgroundSwap();
+
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -35,14 +43,30 @@ public class World_Movement : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             transform.DOMoveY(target.position.y, speed).SetEase(Ease.InSine).OnStart(() => { isAnimating = true; }).OnComplete(() => { isAnimating = false; isLeft = true; }).SetId("World");
-
-
         }
 
         if (Input.GetMouseButtonDown(1))
         {
             transform.DOMoveY(target.position.y, speed).SetEase(Ease.InSine).OnStart(() => { isAnimating = true; }).OnComplete(() => { isAnimating = false; isLeft = true; }).SetId("World");
-
         }
+    }
+
+    void BackgroundSwap()
+    {
+        if (Fever.EnoughSize == true)
+        {
+            BackGround.SetActive(false);
+            FeverBackGround.SetActive(true);
+
+            FeverWalls.SetActive(true);
+        }
+        else
+        {
+            BackGround.SetActive(true);
+            FeverBackGround.SetActive(false);
+
+            FeverWalls.SetActive(false);
+        }
+
     }
 }
